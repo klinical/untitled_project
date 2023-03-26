0182a38d4ded0bf4a2aab5f1bbefa5086d60f23f
Zachary Nickels <zachary.nickels@clearfly.net>
2023-03-26 00:47:36 -0500

    add animation to swim

--- src/aquarium/fish/state/Idle.gd
+++ src/aquarium/fish/state/Idle.gd
@@ -2,4 +2,6 @@ extends FishState
 
 # Called when the node enters the scene tree for the first time.
 func enter(_msg := {}) -> void:
+	fish.get_node("AnimatedSprite2D").play("idle")
+	
 	fish.move_to_new_location()
--- src/aquarium/fish/state/Moving.gd
+++ src/aquarium/fish/state/Moving.gd
@@ -1,16 +1,23 @@
 extends FishState
 
 var target_location: Vector2
+var direction: Vector2
 var point_closeness = Vector2(2, 2)
 
 func enter(msg := {}) -> void:
 	target_location = Vector2(msg.x, msg.y)
+	var sprite: AnimatedSprite2D = fish.get_node("AnimatedSprite2D")
+	direction = (target_location - fish.position).normalized()
+	if direction.x < 0:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+	
+	sprite.play("swim")
 
 
 func _physics_process(delta):
 	if (fish.position.distance_to(target_location) > 2):
-		var direction = (target_location - fish.position).normalized()
-			
 		fish.position += Vector2(
 			(delta * fish.speed_raw) * direction.x, 
 			(delta * fish.speed_raw) * direction.y
